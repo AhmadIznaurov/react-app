ade8e5355996cc7daaa4bc67b899534cbb42afc1
AhmadIznaurov <Ahmad.iznaurov@mail.ru>
2023-03-02 15:08:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
-        <Profile />
+      <div className="app-wrapper-content">
+          <Profile />
+      </div>
+        {/*<Profile />*/}
         <Navbar />
         <Footer />
     </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import Myposts from "./Mypost/Myposts";
 
 const Profile = () => {
     return (
-        <div className={P.content}>
+        <div>
             <div> <img src="https://million-wallpapers.ru/wallpapers/5/10/18230078710629953792/morskoj-zakat-volny.jpg"/>
             </div>
             <div>
