1542282d1b54fc0a27203529430f11cd4a54031f
AhmadIznaurov <Ahmad.iznaurov@mail.ru>
2023-03-04 13:13:55 +0300

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import N from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={N.nav}>
-            <ul>
-                <li><a href="/profile">Profile</a></li>
-                <li className={`${N.item} ${N.active}`}><a href="/dialogs">Messages</a></li>
-                <li><a href="/news">News</a></li>
-                <li><a href="/music">Music</a></li>
-                <br /> <li><a href="/settings">Settings</a></li>
-            </ul>
+            <div className={N.item}>
+                <ul>
+                    <li><NavLink to="/profile" className = { navData => navData.isActive ? N.active : N.item }>
+                        Profile
+                    </NavLink></li>
+                    <li className={`${N.item} ${N.active}`}>
+                        <NavLink to="/dialogs" className = { navData => navData.isActive ? N.active : N.item }>
+                            Messages</NavLink></li>
+                    <li><NavLink to="/news" className = { navData => navData.isActive ? N.active : N.item }>
+                        News</NavLink></li>
+                    <li><NavLink to="/music" className = { navData => navData.isActive ? N.active : N.item }>
+                        Music</NavLink></li>
+                    <br /> <li><NavLink to="/settings" className = { navData => navData.isActive ? N.active : N.item }>
+                    Settings</NavLink></li>
+                </ul>
+            </div>
         </nav>
     );
 }
